7397b9d4703023b98e87a3a0b169ba22ad22bfb2
Flávio Ferreira <business.flavioferreira@gmail.com>
2021-09-06 08:13:02 -0300

    preparing redux do get data from api

---
+++ src/reducers/PokemonListReducer.js
@@ -0,0 +1,13 @@
+const DefaultState = {
+  loading: false,
+  data: []
+}
+
+const PokemonListReducer = (state = DefaultState, action) => {
+  switch(action.type) {
+    default:
+      return state
+  }
+}
+
+export default PokemonListReducer;
